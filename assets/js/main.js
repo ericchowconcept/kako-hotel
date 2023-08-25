e756a5458c83b5ce155c321442faf632de1ca0e8
Eric <ericchow8@gmail.com>
2023-08-25 17:54:25 +0200

    gallery page + responsive done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,14 @@
 const menuBurger = document.querySelector('.burger');
-const menuNav = document.querySelector('.primary-navigation');
+const menuNav = document.querySelector('.overlay');
+const btn = document.querySelector('.btn-header');
+const logo = document.querySelector('.logo');
 
 menuBurger.addEventListener('click', ()=>{
-    menuNav.classList.toggle('menu-mobile')
+
+    logo.style.textAlign = 'center';
+    btn.style.display = 'none';
+    menuNav.classList.toggle('hide');
+    menuBurger.classList('cross');
 })
 
 //*header when scroll
@@ -138,43 +144,146 @@ dotsNav.addEventListener('click', e => {
 
 })   
 
-//*overlay carousel
-//store all of the images in variable image
-const images = document.querySelectorAll('.img-responsive');
 
-//run a foreach through each image
-images.forEach((img)=> {
-    img.addEventListener('click', (e)=> {
-        //store src of the image in imgSrc
-        let imgSrc = e.target.src;
-        imgModal(imgSrc);
+//*links for the image gallery
+
+const galleryLinks = document.querySelectorAll('.gallery-link');
+const galleries = document.querySelectorAll('.image-gallery');
+
+galleryLinks.forEach(link => {
+    link.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        //Deactivate all links and hide all galleries
+        galleryLinks.forEach(link => link.classList.remove('active'));
+        galleries.forEach(gallery => gallery.classList.remove('active'));
+
+        //Activate the clicked link and show the corresponding gallery
+        link.classList.add('active');
+        
+        const target = link.getAttribute('data-target');
+        document.getElementById(`${target}-gallery`).classList.add('active');
     })
 })
 
-//creating the modal
+//*overlay carousel
 
-let imgModal = (src) => {
-    const modal = document.createElement("div");
-    modal.classList.add('modal');
-    //add the modal to the parent element with <div class="image-gallery">
-    document.querySelector('.image-gallery').append(modal);
-    
-    //adding image to modal
-    const newImage = document.createElement("img");
-    newImage.setAttribute("src", src);
-    
-    
-    //creating the close button
-    const closeBtn = document.createElement("i");
-    closeBtn.setAttribute("class", "bi bi-x closeBtn");
-    
-    //close function
-    closeBtn.addEventListener('click', ()=> {
-        modal.remove();
+galleries.forEach((gallery, galleryIndex)=>{
+    //div wrapper that covers the images
+    const imageWrappers = gallery.querySelectorAll('.img-wrapper');
+    //pop up modal with semi black background
+    const modal = gallery.querySelector('.modal');
+
+    //image within
+    const modalImages = modal.querySelector('.modal-content');
+
+    //close button
+    const closeBtn = modal.querySelector('.closeBtn');
+    //next button
+    const nextBtn = modal.querySelector('.nextButton');
+    //previous button
+    const prevBtn = modal.querySelector('.prevButton');
+
+    // Attach click event listeners to image wrappers
+    imageWrappers.forEach((wrapper,index)=> {
+        wrapper.addEventListener('click', ()=>{
+            openModal(galleryIndex, index);
+        })
     })
+    
+    // Other event listeners and functions remain the same with adjustments to modal handling
+    closeBtn.addEventListener('click', () => {
+        closeModal();
+    });
 
-    modal.append(newImage, closeBtn);
-}
+    prevBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex - 1 + imageWrappers.length) % imageWrappers.length;
+        updateModalImage();
+    });
+
+    nextBtn.addEventListener('click', () => {
+        currentIndex = (currentIndex + 1) % imageWrappers.length;
+        updateModalImage();
+    });
+
+    function openModal(galleryIndex, imageIndex) {
+        currentIndex = imageIndex;
+        updateModalImage();
+        modal.style.display = 'flex';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+
+    function updateModalImage() {
+        const selectedGallery = galleries[galleryIndex];
+        const selectedImageWrapper = selectedGallery.querySelectorAll('.img-wrapper')[currentIndex];
+        modalImages.src = selectedImageWrapper.querySelector('img').src;
+    }
+
+    // Close modal when clicking outside the image
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            closeModal();
+        }
+    });
+
+})
+
+
+// // Attach click event listeners to image wrappers
+
+// imageWrappers.forEach((wrapper, index) => {
+//     wrapper.addEventListener('click', () => {
+//         imgModal(imageElements[index].src, index);
+//     });
+// });
+
+// //creating the modal
+
+// let imgModal = (src, currentIndex) => {
+//     const modal = document.createElement("div");
+//     modal.classList.add('modal');
+//     //add the modal to the parent element with <div class="image-gallery">
+//     document.querySelector('.image-gallery').append(modal);
+    
+//     //adding image to modal
+//     const newImage = document.createElement("img");
+//     newImage.setAttribute("src", src);
+    
+    
+//     //creating the close button
+//     const closeBtn = document.createElement("i");
+//     closeBtn.setAttribute("class", "bi bi-x closeBtn");
+
+//     //creating the prev, next button
+//     const prevBtn = document.createElement("i");
+//     prevBtn.setAttribute("class", "bi bi-caret-left-fill prevButton");
+
+//     const nextBtn = document.createElement("i");
+//     nextBtn.setAttribute("class", "bi bi-caret-right-fill nextButton");
+
+    
+//     //close function
+//     closeBtn.addEventListener('click', ()=> {
+//         modal.remove();
+//     })
+
+//     prevBtn.addEventListener('click', () => {
+//         currentIndex = (currentIndex - 1 + imageElements.length) % imageElements.length;
+//         newImage.src = imageElements[currentIndex].src;
+//     });
+    
+//     nextBtn.addEventListener('click', () => {
+//         currentIndex = (currentIndex + 1) % imageElements.length;
+//         newImage.src = imageElements[currentIndex].src;
+//     });
+
+
+//     modal.append(newImage, closeBtn, prevBtn, nextBtn);
+
+// }
 
 
 
